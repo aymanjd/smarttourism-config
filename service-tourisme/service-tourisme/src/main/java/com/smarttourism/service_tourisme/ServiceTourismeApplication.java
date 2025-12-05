3bbf82ceee60e8805178138874715960fc51e74e
ayman jd <ayman.jeddi10-github@gmail.com>
2025-12-05 22:30:15 +0100

    Initial commit - SmartTourism project

---
+++ service-tourisme/service-tourisme/src/main/java/com/smarttourism/service_tourisme/ServiceTourismeApplication.java
@@ -0,0 +1,12 @@
+package com.smarttourism.service_tourisme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTourismeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTourismeApplication.class, args);
+	}
+}
---
+++ service-tourisme/service-tourisme/src/test/java/com/smarttourism/service_tourisme/ServiceTourismeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttourism.service_tourisme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTourismeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
